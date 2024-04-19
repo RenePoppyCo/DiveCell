4e66503db4598f3590a9221b1d61e781e1148f49
Rene <68439248+Rene-goon@users.noreply.github.com>
2024-04-19 11:12:17 -0500

    added camera script to follow player

--- DiveCell/Assets/CameraFollow.cs
+++ DiveCell/Assets/CameraFollow.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    [SerializeField] private float followSpeed = 0.1f;
+    [SerializeField] private Vector3 offset;
+ 
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,6 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.Lerp(transform.position, PlayerController.Instance.transform.position + offset, followSpeed);
     }
 }
